f08b7b64b2c061912a2e73ce4d4e6e616811e179
jwumanji <justin@roxollo.com>
2013-08-02 18:35:47 -0700

    Add Product association

--- app/models/product.rb
+++ app/models/product.rb
@@ -1,3 +1,9 @@
 class Product < ActiveRecord::Base
-  attr_accessible :description
+  attr_accessible :description, :title
+
+  validates :description, presence: true
+  validates :title, presence: true
+  validates :user_id, presence: true
+
+  belongs_to :user
 end
--- db/migrate/20130802231909_add_user_idto_products.rb
+++
@@ -1,7 +0,0 @@
-class AddUserIdtoProducts < ActiveRecord::Migration
-  def up
-  end
-
-  def down
-  end
-end
